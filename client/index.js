4f699e45a648c693957cbb139ae6a6e58e758810
Nguyễn Xuân Tiến <xuantien.nguyen93@gmail.com>
2020-08-27 11:12:02 +0700

    complete game logic

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
-const socket = io('http://127.0.0.1:3000');
+var host = window.location.href
+host = host.includes('tientm.com') ? 'http://easter-egg-extreme.tientm.com' : 'http://127.0.0.1:3000'
+console.log(host);
+const socket = io(host);
 
 const mapKeyCodeToAction = {
     37: 'left',
@@ -9,26 +12,50 @@ const mapKeyCodeToAction = {
 const ratio = 10
 var playerId = null
 var updateTime = 50
+var gameDuration = 0
+var timer = null
 
 socket.on('setup', data => {
     playerId = data.playerId
     updateTime = data.updateTime
+    gameDuration = data.gameDuration
+    var totalSecond = gameDuration / 1000
+    var minute = Math.floor(totalSecond / 60)
+    var second = totalSecond % 60
+    $('#timer').html(`${minute}:${second}`)
     for (var id of data.idList) {
         var color = id === data.playerId ? 'red' : 'black'
         var zIndex = id === data.playerId ? 100 : 99
         $('#board').append(`<div id="${id}" class="player" style="background:${color};z-index:${zIndex}"></div>`)
+        $('#score-container').append(`<div id="score-${id}" class="player-score" style="color:${color}">Player ${id}: 0</div>`)
     }
+    $('#egg').css({
+        top: data.egg.y * ratio,
+        left: data.egg.x * ratio
+    })
 })
 
 socket.on('state', playerMap => {
     for (var i in playerMap) {
-        var player = $('#' + i)
-        var position = playerMap[i]
-        player.animate({
-            top: position.y * ratio,
-            left: position.x * ratio
+        var playerEl = $('#' + i)
+        var player = playerMap[i]
+        playerEl.animate({
+            top: player.y * ratio,
+            left: player.x * ratio
         }, updateTime)
+        $('#score-' + i).html(`Player ${i}: ${player.score}`)
     }
+})
+
+socket.on('egg', egg => {
+    $('#egg').css({
+        top: egg.y * ratio,
+        left: egg.x * ratio
+    })
+})
+
+socket.on('stop', egg => {
+    clearInterval(timer)
 })
 
 $('body').keydown(event => {
@@ -39,7 +66,31 @@ $('body').keydown(event => {
 })
 
 function start() {
+    $('#start').css('display', 'none')
+    $('#stop').css('display', 'block')
     socket.emit('start')
+    var totalSecond = gameDuration / 1000
+    var minute = Math.floor(totalSecond / 60)
+    var second = totalSecond % 60
+    timer = setInterval(() => {
+        second--
+        if (second < 0) {
+            minute--
+            if (minute < 0) {
+                minute = 0
+                clearInterval(timer)
+            } else {
+                second = 60
+            }
+        }
+        $('#timer').html(`${minute}:${second < 10 ? '0' + second : second}`)
+    }, 1000)
+}
+
+function stop() {
+    $('#stop').css('display', 'none')
+    $('#start').css('display', 'block')
+    socket.emit('stop')
 }
 
 socket.emit('setup')
--- server/socket.js
+++ server/socket.js
@@ -1,18 +1,31 @@
+const { clearInterval } = require('timers');
+
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 const numberOfPlayer = 3
 const updateTime = 50
+const gameDuration = (1 * 60 + 30) * 1000
+const boardSize = {
+    w: 80,
+    h: 80
+}
+
+function getEgg() {
+    return {
+        x: Math.floor(Math.random() * boardSize.w),
+        y: Math.floor(Math.random() * boardSize.h)
+    }
+}
 
 io.on('connection', client => {
-    var direction = 'down'
-    var boardSize = {
-        w: 80,
-        h: 80
-    }
+    console.log('connected')
+
     var playerMap = {}
     var updateInterval = null
+    var statusTimeout = null
     var playerId = '1'
-    console.log('connected')
+    var egg = getEgg()
+    var gameStatus = false
     client.on('setup', () => {
         let idList = []
         for (let i = 1; i <= numberOfPlayer; i++) {
@@ -22,37 +35,46 @@ io.on('connection', client => {
                 id: id,
                 direction: 'down',
                 x: 0,
-                y: 0
+                y: 0,
+                score: 0
             }
         }
         client.emit('setup', {
             idList: idList,
             playerId: '1',
-            updateTime: updateTime
+            updateTime: updateTime,
+            egg: egg,
+            gameDuration: gameDuration
         })
     })
 
-    function getRandomDirection() {
-        let random = Math.floor(Math.random() * 4)
-        switch (random) {
-            case 0: return 'up'
-            case 1: return 'right'
-            case 2: return 'down'
-            case 3: return 'left'
+    function botMove(id) {
+        if (!gameStatus) {
+            return
         }
-    }
-
-    function botMove(id, direction) {
-        playerMap[id].direction = direction
+        var player = playerMap[id]
+        var directionList = []
+        if (player.y !== egg.y) {
+            directionList.push(player.y < egg.y ? 'down' : 'up')
+        }
+        if (player.x != egg.x) {
+            directionList.push(player.x < egg.x ? 'right' : 'left')
+        }
+        player.direction = directionList.length < 1 ? player.direction : directionList[Math.floor(Math.random() * directionList.length)]
         setTimeout(() => {
-            botMove(id, getRandomDirection())
+            botMove(id)
         }, Math.random() * 400 + 100)
     }
 
     client.on('start', () => {
+        gameStatus = true
+        statusTimeout = setTimeout(() => {
+            gameStatus = false
+            clearInterval(updateInterval)
+        }, gameDuration)
         for (var i in playerMap) {
             if (i !== playerId) {
-                botMove(i, getRandomDirection())
+                botMove(i)
             }
         }
         updateInterval = setInterval(() => {
@@ -73,6 +95,16 @@ io.on('connection', client => {
                         break
                 }
             }
+            for (var i in playerMap) {
+                var player = playerMap[i]
+                if (player.x === egg.x && player.y === egg.y) {
+                    player.score++
+                    console.log(`player ${i}: ${player.score}`);
+                    egg = getEgg()
+                    client.emit('egg', egg)
+                    break
+                }
+            }
             client.emit('state', playerMap)
         }, updateTime)
     });
@@ -80,11 +112,18 @@ io.on('connection', client => {
         if (playerMap[data.id]) {
             playerMap[data.id].direction = data.direction
         }
-    
+
+    });
+    client.on('stop', data => {
+        gameStatus = false
+        clearInterval(updateInterval)
+        clearTimeout(statusTimeout)
+        client.emit('stop')
     });
     client.on('disconnect', () => {
         console.log('disconnect')
         clearInterval(updateInterval)
+        clearTimeout(statusTimeout)
     });
 });
 
